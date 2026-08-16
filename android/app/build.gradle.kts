5254ab34c5a1ec0957e98604b98f5c806967b730
merci1994dz <2.03042494e+08+merci1994dz@users.noreply.github.com>
2026-08-16 07:33:32 +0000

    cleanup: remove legacy splash branding asset

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -212,9 +212,6 @@ dependencies {
     // QR Code generation
     implementation("com.google.zxing:core:3.5.3")
 
-    // Lottie animation (splash screen)
-    implementation("com.airbnb.android:lottie-compose:6.4.0")
-
     // Testing
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
