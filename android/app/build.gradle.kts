33d471066d1e2e18d7b75d844b2c631596f5e119
mostafabonnif-beep <2.83277468e+08+mostafabonnif-beep@users.noreply.github.com>
2026-08-27 02:58:14 +0000

    chore(android): prepare client 2 mobile review 1.0.19

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         val buildVersionName = if (project.hasProperty("versionName")) {
             project.property("versionName") as String
         } else {
-            "1.0.18"
+            "1.0.19"
         }
         val buildVersionParts = buildVersionName
             .trim()
