499387f299927e403cfad716f4859a6145f485c0
mostafabonnif-beep <mostafabonnif-beep@users.noreply.github.com>
2026-09-04 20:41:28 +0000

    fix(tv): compile catalog controls on Android

--- android/app/src/main/java/com/dzhoof/iptv/presentation/ui/screens/CatalogScreen.kt
+++ android/app/src/main/java/com/dzhoof/iptv/presentation/ui/screens/CatalogScreen.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -151,7 +153,7 @@ private fun CatalogToolbar(
     onSearch: () -> Unit,
     isCompact: Boolean,
 ) {
-    val content: @Composable () -> Unit = {
+    val content: @Composable RowScope.() -> Unit = {
         FilterChip(
             selected = tab == CatalogTab.MOVIES,
             onClick = { onTabSelected(CatalogTab.MOVIES) },
