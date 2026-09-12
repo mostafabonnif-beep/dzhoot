09e43f8d1f0ddff7fe2d400e8a1ea0eb4481b5a3
zocomputer <help@zocomputer.com>
2026-09-12 01:41:37 +0000

    chore(android): bump version to 1.2.6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         val buildVersionName = if (project.hasProperty("versionName")) {
             project.property("versionName") as String
         } else {
-            "1.2.5"
+            "1.2.6"
         }
         val buildVersionParts = buildVersionName
             .trim()
